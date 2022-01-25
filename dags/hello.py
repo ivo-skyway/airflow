07c3aaa5e4e0b337d6cdf7240eed4acb2dc8d490
ivo <ivo@hologram.com>
2022-01-25 07:38:29 -0800

    v0

--- dags/hello.py
+++ dags/hello.py
@@ -3,23 +3,25 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
+now = datetime.now()
+cron = '0 * * * *'  # execute every hour at :00
+cron = '*/5 * * * *'  # execute every 5th minute
+start = now
+version = "0.1.25.0"
+
 
 def print_hello():
-    msg = f'Hello world, time now: {datetime.now()}'
+    msg = f'Hello world v.{version} time now: {datetime.now()}'
     print(msg)
     return msg
 
 
-now = datetime.now()
-cron = '0 * * * *'  # execute every hour at :00
-cron = '* /5 * * *'  # execute every 5th minute
-start = now
-dag = DAG('hello_world', description='Hello World DAG',
+dag = DAG('hello_world', description=f'Hello World DAG v.{version}',
           schedule_interval=cron,
           start_date=start, catchup=False)
 
 hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
 
-print(f'starting: {now}, schedule: {cron}, start: {now}')
+print(f'Hello v.{version} starting: {now}, schedule: {cron}, start: {now}')
 
 hello_operator
