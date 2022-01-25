298cf2529ba291ea51fbe8e40091e9fec1511719
ivo <ivo@hologram.com>
2022-01-25 06:14:31 -0800

    wip

--- fernet_key.py
+++
@@ -1,8 +0,0 @@
-import secrets
-
-from cryptography.fernet import Fernet
-
-fernet_key = Fernet.generate_key()
-print(fernet_key.decode())  # your fernet_key, keep it in secured place!
-
-print(secrets.token_hex(16))
