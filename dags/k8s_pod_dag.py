048ad2c794f6a22e334f267fa9077b59fbdd06e5
ivo <ivo@hologram.com>
2022-01-26 15:33:37 -0800

    docker

--- dags/k8s_pod_dag.py
+++ dags/k8s_pod_dag.py
@@ -1,49 +1,42 @@
-from datetime import datetime, timedelta
+import datetime as dt
 
 from airflow import DAG
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
 
-version = "0.1.25.7"
-
-# k = KubernetesPodOperator(
-#     name="hello-k8s",
-#     image="python:3.8-slim-buster",
-#     cmds=["bash", "-cx"],
-#     arguments=["echo", "10"],
-#     labels={"foo": "bar"},
-#     task_id="kube_demo",
-#     do_xcom_push=True,
-# )
-# k.dry_run()
+version = "0.1.26.0"
 
 default_args = {
     'owner': 'Ivo',
-    'start_date': datetime(2022, 1, 1),
-    'retries': 1,
-    'retry_delay': timedelta(seconds=5)
+    'start_date': dt.datetime(2022, 1, 1),
+    'retries': 3,
+    'retry_delay': dt.timedelta(seconds=30)
 }
 
 with DAG('etl_dag',
          default_args=default_args,
          catchup=False,
-         schedule_interval=None) as dag:
-    extract_transform = KubernetesPodOperator(
-        namespace='default',
-        image="python:3.8-slim-buster",
-        cmds=["sleep"],
-        arguments=["100"],
+         schedule_interval=dt.timedelta(seconds=60)) as dag:
+    etl = KubernetesPodOperator(
+        namespace='airflow',
+        image="docker.io/ivostoy/my-dbt:1.0.0",
+        cmds=["--version"],
+        arguments=[],
         labels={"foo": "bar"},
-        name="extract-transform",
-        task_id="extract-transform",
+        name="etl",
+        task_id="etl",
         get_logs=True,
         log_events_on_failure=True,
         in_cluster=True,
     )
 
-    print(f'k8s_pod v.{version} start,  {datetime.now()}')
+    print(etl.dry_run())
 
-    extract_transform
+    print(f'k8s_pod etl v.{version} start,  {dt.datetime.now()}')
 
-    print(f'k8s_pod end  {datetime.now()}')
+    etl
+
+    dt.time.sleep(10)
+
+    print(f'k8s_pod end  {dt.datetime.now()}')
